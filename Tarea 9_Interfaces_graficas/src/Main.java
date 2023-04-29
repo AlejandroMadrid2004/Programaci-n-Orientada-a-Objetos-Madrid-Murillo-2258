f0dda1c0a0c524d2a5678e641443a2e02cd4fbdd
AlejandroMadrid2004 <125769418+AlejandroMadrid2004@users.noreply.github.com>
2023-04-29 14:21:23 -0600

    Add files via upload

---
+++ Tarea 9_Interfaces_graficas/src/Main.java
@@ -0,0 +1,9 @@
+import iu.Principal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Principal ventana = new Principal("Hola mundo AWT");
+    }
+}
