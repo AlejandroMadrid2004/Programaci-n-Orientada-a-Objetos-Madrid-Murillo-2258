2cc63c37f927580ef3a773a35113e1d4f6703f5f
AlejandroMadrid2004 <125769418+AlejandroMadrid2004@users.noreply.github.com>
2023-05-03 23:51:57 -0600

    Add files via upload

---
+++ Tarea_Extra3_Layouts/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGrind ventanaGrind = new VentanaGrind("Ventana Layout cuadriculada");
+
+
+
+
+    }
+}
