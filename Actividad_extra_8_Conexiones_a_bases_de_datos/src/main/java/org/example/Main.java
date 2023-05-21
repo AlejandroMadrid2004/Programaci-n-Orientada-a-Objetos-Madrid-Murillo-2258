8f7a291bfbdb2815fb09f97b6758a885771855d6
AlejandroMadrid2004 <125769418+AlejandroMadrid2004@users.noreply.github.com>
2023-05-21 14:38:41 -0600

    Add files via upload

---
+++ Actividad_extra_8_Conexiones_a_bases_de_datos/src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.example.controlador.ControladorLibro;
+import org.example.modelo.Libro;
+import org.example.persistencia.LibroDAO;
+import org.example.vista.VentanaLibro;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        VentanaLibro view = new VentanaLibro("MVC y JDBC");
+        ControladorLibro controller = new ControladorLibro(view);
+
+    }
+}
